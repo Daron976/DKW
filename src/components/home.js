730471d955ba503d17aee20c17052be842b277e2
Daron976 <daronkwiafe@gmail.com>
2023-03-20 09:35:54 +0200

    Fix homepage animation

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Home = () => (
-  <section className="home-content flex" data-testid="home">
-    <div className="home-header">
-      <h1>Daron Wiafe</h1>
-      <p className="subtext">Software developer</p>
-    </div>
-  </section>
-);
+const Home = () => {
+  const [animation, setAnimation] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (animation) {
+        setAnimation(false);
+      }
+    }, 2000);
+  }, []);
+
+  return (
+    <section className="home-content flex" data-testid="home">
+      <div className="home-header">
+        <h1
+          className={
+            animation
+              ? 'name-animation'
+              : ''
+          }
+        >
+          Daron Wiafe
+        </h1>
+        <p
+          className={
+            animation
+              ? 'subtext-animation subtext'
+              : 'subtext'
+          }
+        >
+          Software developer
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default Home;
